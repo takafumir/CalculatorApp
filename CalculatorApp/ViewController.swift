0ca0d7f834803c831c902b4ec38e72167559c9dd
Takafumi Yamano <takafumiyamano@gmail.com>
2017-04-25 18:35:39 +0900

    initial commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Yamano Takafumi on 2017/04/25.
+//  Copyright © 2017 takafumi.yamano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
