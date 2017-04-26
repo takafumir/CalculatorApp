914f0a235e9742b46a1de31c7478ac364e9486ee
Takafumi Yamano <takafumiyamano@gmail.com>
2017-04-26 19:05:39 +0900

    implement clearCalculation, calculateAnswer actions
    fix regexp for formatFormula

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Expression
 
 class ViewController: UIViewController {
     @IBOutlet weak var formulaLabel: UILabel!
@@ -21,7 +22,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func inputFormula(_ sender: UIButton) {
@@ -35,10 +35,52 @@ class ViewController: UIViewController {
         formulaLabel.text = formulaText + senderedText
     }
 
-    @IBAction func calculateAnswer(_ sender: UIButton) {
+    @IBAction func clearCalculation(_ sender: UIButton) {
+        // `C`ボタンが押されたら式と答えをクリアする
+        formulaLabel.text = ""
+        answerLabel.text = ""
     }
 
-    @IBAction func clearCalculation(_ sender: UIButton) {
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+        // `=`ボタンが押されたら答えを計算して表示する
+        guard let formulaText = formulaLabel.text else {
+            return
+        }
+        let formula: String = formatFormula(formulaText)
+        answerLabel.text = evalFormula(formula)
+    }
+
+    private func formatFormula(_ formula: String) -> String {
+        // 入力された整数には`.0`を追加して小数として評価する
+        // また`÷`を`/`に、`×`を`*`に置換する
+        let formattedFormula: String = formula.replacingOccurrences(
+                of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+                with: "$1.0",
+                options: NSString.CompareOptions.regularExpression,
+                range: nil
+            ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        return formattedFormula
+    }
+
+    private func evalFormula(_ formula: String) -> String {
+        do {
+            // Expressionで文字列の計算式を評価して答えを求める
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        } catch {
+            // 計算式が不当だった場合
+            return "式を正しく入力してください"
+        }
+    }
+
+    private func formatAnswer(_ answer: String) -> String {
+        // 答えの小数点以下が`.0`だった場合は、`.0`を削除して答えを整数で表示する
+        let formattedAnswer: String = answer.replacingOccurrences(
+                of: "\\.0$",
+                with: "",
+                options: NSString.CompareOptions.regularExpression,
+                range: nil)
+        return formattedAnswer
     }
 }
-
